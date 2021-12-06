c3e8ec476c191db87117770891c35b87d3935b91
Márcia Tiemi Matsumoto <tiemi.matsumoto@gmail.com>
2021-12-06 18:25:37 +0000

    pushing non-flaky changes

--- cypress/integration/accessConsular.spec.js
+++ cypress/integration/accessConsular.spec.js
@@ -5,8 +5,9 @@ describe("Access to Consular", () => {
 
   it("should check scheduling dates", () => {
     cy.login(Cypress.env("email"), Cypress.env("pass"));
-    cy.wait(5000);
+    cy.get(".container .title", { timeout: 10000 }).should('be.visble');
     cy.get(".row .col .btn-outline-primary").contains('Continuar').click();
+    cy.get(".container .title", { timeout: 10000 }).should('be.visble');
     cy.get(".alert-primary").should("have.text", "Não há horários disponíveis no momento. Tente novamente mais tarde!");
   });
 });
